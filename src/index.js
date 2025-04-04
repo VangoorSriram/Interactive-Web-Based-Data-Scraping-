d2cebf8a351019ceb928fe35a385a353bb64f37e
VangoorSriram <svangoor@my.okcu.edu>
2025-04-03 19:50:51 -0500

    Update index.js
    week 12

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import createRoot from react-dom/client
-import ScraperForm from './components/ScraperForm';
-import Dashboard from './components/Dashboard';
-import "./App.css";
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Make sure this imports from App.js
+import './App.css';
 
-function App() {
-    return (
-        <div>
-            <h1>Web Scraper</h1>
-            <ScraperForm />
-            <Dashboard />
-        </div>
-    );
-}
-
-// Use createRoot instead of ReactDOM.render()
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
